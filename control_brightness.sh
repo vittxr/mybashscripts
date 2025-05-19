233eef649761353bb9ff8c087491568c39ef18e5
Vitor Schirmer <vitor.roberto3022@gmail.com>
2025-05-18 21:42:19 -0300

    Update control_brightness.sh

--- control_brightness.sh
+++ control_brightness.sh
@@ -2,3 +2,4 @@
 xrandr --output HDMI-1-0 --brightness 0.7
 
 # hdmi-1-0 is the target monitor (list the monitor using: xrandr --listactivemonitors)
+# install 'control-brightness' is easier (https://www.geeksforgeeks.org/how-to-control-screen-brightness-in-ubuntu-22-04/)
