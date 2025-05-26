2db141b72fb9da3114ffed1b3a0d3603c84f4936
Vitor Schirmer <vitor.roberto3022@gmail.com>
2025-05-25 21:44:53 -0300

    ignore zip files.

--- rclone_gdrive_sync.sh
+++ rclone_gdrive_sync.sh
@@ -1,17 +1,36 @@
+# -> create
 #!/bin/bash
 DIR="/home/vitor/Desktop"
-rclone --config=/home/vitor/.config/rclone/rclone.conf sync "$DIR" myubuntu:Desktop --exclude 'node_modules/**' --exclude 'venv/**'
+rclone --config=/home/vitor/.config/rclone/rclone.conf sync "$DIR" myubuntu:Desktop \
+  --exclude 'node_modules/**' \
+  --exclude 'venv/**' \
+  --exclude '*.zip' \
+  --exclude '*.tar.gz' \
+  --exclude '*.sql.gz'
+
 
 # To create the service, you need to run sudo -i to run commands as root
-# 1. Create the service to keep this file running (cannot run a service where the binary is in a user's home directory, or in our case, the root user's home directory. We need to use /usr/local/bin - https://serverfault.com/questions/957084/failed-at-step-exec-spawning-permission-denied:  
+# 1. Create the service file in (/etc/systemd/system/gdrive_sync.service) to keep this file running (cannot run a service where the binary is in a user's home directory, or in our case, the root user's home directory. We need to use /usr/local/bin - https://serverfault.com/questions/957084/failed-at-step-exec-spawning-permission-denied:  
   # [Unit]
   # Description=Auto sync to google drive       
 
   # [Service]
-  # ExecStart=/usr/local/bin/gdrive_sync.sh  
+  # ExecStart=/usr/local/bin/gdrive_sync.sh # ->  
 
   # [Install]
   # WantedBy=multi-user.target
-# 2. Enable the service: sudo systemctl enable gdrive_sync.service
-# 3. Start the service: sudo systemctl start gdrive_sync.service
-# 4. Check the status of the service: sudo systemctl status gdrive_sync.service
+  
+# 2. Create `/usr/local/bin/gdrive_sync.sh` file:
+  # DIR="/home/vitor/Desktop"
+  # rclone --config=/home/vitor/.config/rclone/rclone.conf sync "$DIR" myubuntu:Desktop \
+  #  --exclude 'node_modules/**' \
+  #  --exclude 'venv/**' \
+  #  --exclude '*.zip' \
+  #  --exclude '*.tar.gz' \
+  #  --exclude '*.sql.gz' 
+  
+# 3. Enable the service: sudo systemctl enable gdrive_sync.service
+
+# 4. Start the service: sudo systemctl start gdrive_sync.service
+
+# 5. Check the status of the service: sudo systemctl status gdrive_sync.service
